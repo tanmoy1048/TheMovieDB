f0025843f749d6bbf7b70dc3e30e76c8639c4693
Rafi Sadat <rafi.sadat@act.gov.au>
2020-11-11 23:19:41 +1100

    jetpack navigation is added.

---
+++ app/src/main/java/com/seven/assignment/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.seven.assignment.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.seven.assignment.R
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.main_nav_host_fragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp() =
+        navController.navigateUp() || super.onSupportNavigateUp()
+}
